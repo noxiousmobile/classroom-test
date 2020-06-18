fa4061cc4ecc8d7952e4d0ec5197ca32c025bea0
Nikola Dimitrov <nikola.dimitrov@lottomart.com>
2020-06-18 21:03:09 +0200

    TASK1, TASK2 and TASK4(Partialy)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClassroomSelectorComponent } from './components/classroom-selector/classroom-selector.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassroomSelectorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
